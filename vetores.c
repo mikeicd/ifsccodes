20988ca0464522fe699eaf0a15f31c51038280a5
Mike <mikeicd@gmail.com>
2019-08-22 23:06:09 -0300

    Alteração na variavel pEscalar!!

--- vetores.c
+++ vetores.c
@@ -2,21 +2,23 @@
 
 int main()
 {
-    int ax,ay,bx,by,produto;
+    int ax,ay,bx,by,pEscalar;
 
-    printf("Informe as coordenadas de do vetor A:\n")
-    printf("X:")
-    scanf(%d,&ax)
-    printf("Y:")
-    scanf(%d,&ay)
+    printf("Informe as coordenadas de do vetor A:\n");
+    printf("X:");
+    scanf("%d",&ax);
+    printf("Y:");
+    scanf("%d",&ay);
 
-    printf("Informe as coordenadas de do vetor B:\n")
-    printf("X:")
-    scanf(%d,&bx)
-    printf("Y:")
-    scanf(%d,&by)
+    printf("Informe as coordenadas de do vetor B:\n");
+    printf("X:");
+    scanf("%d",&bx);
+    printf("Y:");
+    scanf("%d",&by);
 
+    pEscalar=(ax*bx)+(ay*by);
 
+    printf("O produto dos vetores A e B é: %d\n",pEscalar);
 
 return 0;    
 }
