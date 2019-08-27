aa83fbe590df1a2de076e4927cec1ff7dcd911c4
Mike <mikeicd@gmail.com>
2019-08-27 19:31:49 -0300

    Adicionado o executavel do cicle

--- cicle.c
+++ cicle.c
@@ -2,6 +2,20 @@
 
 int main()
 {
-  
+	int alpha,beta,gama,x,y,z;
+
+  printf("Entre com o valor de Alpha:");
+  scanf("%d",&alpha);
+	printf("Entre com o valor de Beta:");
+  scanf("%d",&beta);
+	printf("Entre com o valor de Gama:");
+  scanf("%d",&gama);
+
+	printf("Alpha: %d, Beta: %d, Gama: %d \n",alpha,beta,gama);
+	x=alpha,y=beta,z=gama; //Guardando os valores em novas variaveis para fazer a troca em seguida...
+	beta=x,gama=y,alpha=z;
+	
+	printf("Trocando os valores das variaveis...\nAlpha: %d, Beta: %d, Gama: %d \n",alpha,beta,gama);
+
 return 0;
 }
