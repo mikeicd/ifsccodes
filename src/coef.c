5bf72bbf089d2426be020e4835fe504192fec8f2
Mike <mikeicd@gmail.com>
2019-08-28 18:31:27 -0300

    Adicionado o desafioda aula 4, usei a biblio math.h e as funções sqrtf e powf.

--- src/coef.c
+++ src/coef.c
@@ -2,23 +2,22 @@
 
 int main()
 {
-
- float x1,y1,x2,y2,m,n;
- printf("Insira os valores do ponto P:\n");
- printf("X: ");
- scanf("%f",&x1);
- printf("Y: ");
- scanf("%f",&y1);
- printf("Insira os valores do ponto Q:\n");
- printf("X: ");
- scanf("%f",&x2);
- printf("Y: ");
- scanf("%f",&y2);
+	float x1,y1,x2,y2,m,n;
+	printf("Insira os valores do ponto P:\n");
+	printf("X: ");
+	scanf("%f",&x1);
+	printf("Y: ");
+	scanf("%f",&y1);
+	printf("Insira os valores do ponto Q:\n");
+	printf("X: ");
+	scanf("%f",&x2);
+	printf("Y: ");
+	scanf("%f",&y2);
   
- m=(y2-y1)/(x2-x1); //Calcullo coef Angular
- n=((m*(-x1))+y1);	//Calculo coef Linear
+	m=(y2-y1)/(x2-x1); //Calcullo coef Angular
+	n=((m*(-x1))+y1);	//Calculo coef Linear
  
- printf("Valor do coeficiente angular é:%.2f e o Valor do coefiente linear é: %.2f \n",m,n);
+	printf("Valor do coeficiente angular é:%.2f e o Valor do coefiente linear é: %.2f \n",m,n);
 
-	return 0;
+return 0;
 }
---
+++ src/desafio.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+  float o,u,n1,n2,n3,n4,n5,n6,n7;
+  
+  printf("Determine os 7 valores a serem analizados para o calculo do desvio populacional:\n");
+  printf("1° Valor:");
+  scanf("%f",&n1);
+  printf("2° Valor:");
+  scanf("%f",&n2);
+  printf("3° Valor:");
+  scanf("%f",&n3);
+  printf("4° Valor:");
+  scanf("%f",&n4);
+  printf("5° Valor:");
+  scanf("%f",&n5);
+  printf("6° Valor:");
+  scanf("%f",&n6);
+  printf("7° Valor:");
+  scanf("%f",&n7);
+
+  u=(n1+n2+n3+n4+n5+n6+n7)/7.0;
+  o=sqrtf((powf((n1-u),2.0))+(powf((n2-u),2.0))+(powf((n3-u),2.0))+(powf((n4-u),2.0))+(powf((n5-u),2.0))+(powf((n6-u),2.0))+(powf((n7-u),2.0))/7.0);
+
+  printf("O desvio padrão é de: %f.\n",o);
+
+return 0;
+}
