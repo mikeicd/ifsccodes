3e34bfae5c4adcc499a7ae4c5f2a0ca7ab3ebd03
Mike <mikeicd@gmail.com>
2019-09-11 17:33:06 -0300

    Finalizado o codigo da media de n numeros

--- aula06/src/mediaNumeros.c
+++ aula06/src/mediaNumeros.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main () {
-    int n,soma,val;
+    int n,soma=0,val=0;
 
     printf("Digite a quantidade de numeros: ");
     scanf("%d",&n);
@@ -10,8 +10,8 @@ int main () {
         printf("Informe o %d° numero: ",i);
         scanf("%d",&val);
         soma=soma+val;
-        
-        
+        printf("Soma até o momento: %d\n",soma);   
     }
+    printf("A soma total é:  %d\n",soma);
     return 0;
 }
