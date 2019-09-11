8d1b1fb564f348d7e121de870272c61a1ed2ede4
Mike <mikeicd@gmail.com>
2019-09-11 09:22:57 -0300

    Exer 07, fatoriais m e n

---
+++ aula06/exerIni/exer07.c
@@ -0,0 +1,30 @@
+// Dado dois números inteiros positivos "M" e "N", computar o valor "V" conforme a equação: V=(M!*2.5)/(N!*7)
+
+#include <stdio.h>
+
+int main () {
+    
+    float v;
+    int n,m,fat1,fat2;
+    fat1=fat2=1;
+
+    printf("Insira o valor de M: ");
+    scanf("%d",&m);
+
+    for (int i = 1; i <= m; i++)  {
+        fat1=fat1*i;
+    }
+    printf("O valor de m é : %d\n",fat1);
+
+    printf("Insira o valor de N: ");
+    scanf("%d",&n);
+    for (int i = 1; i <= n; i++)  {
+        fat2=fat2*i;
+    }
+    printf("O valor de n é : %d\n",fat2);
+
+    v=((fat1*2.5)/(fat2*7.0));
+    printf("O valor de V é : %.3f\n",v);
+
+    return 0;
+}
