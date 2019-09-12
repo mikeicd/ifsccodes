1f476a25274354c3bd0dd1e2496e44838ea79bb6
Mike <mikeicd@gmail.com>
2019-09-12 19:35:37 -0300

    ex02redux- alterado a variavel do for da altura para não dar problema com os outros.

--- aula06/exerMedio/src/ex02redux.c
+++ aula06/exerMedio/src/ex02redux.c
@@ -19,8 +19,8 @@ int main () {
   scanf("%d",&m);
 
 
-  for (int i = 1; i <= h; i++) {
-    if (i==1 || i==h) {
+  for (int j = 1; j <= h; j++) {
+    if (j==1 || j==h) {
       for (int i = 1; i <= m; i++) {
         printf(".");
       }
